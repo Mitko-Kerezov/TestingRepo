bf95e6fb1a4efae39b4caf6ed51d646a169b7155
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2018-09-27 20:39:45 +0300

    Do not clear sync

--- app/cmd/cmd-list.component.ts
+++ app/cmd/cmd-list.component.ts
@@ -11,28 +11,28 @@ import { NavigationService } from "../core/services/navigation.service";
 })
 export class CmdListComponent implements OnInit {
     private dataStoreType = Kinvey.DataStoreType.Cache;
-    
+
     public myItems: Observable<any[]>;
-    
+
     constructor(private backendService: BackendService,
                 private navigationService: NavigationService,
                 private activatedRoute: ActivatedRoute) {
     }
-    
+
     ngOnInit(): void {
         this.myItems = this.backendService.find("customers", this.dataStoreType);
         console.log("pendingSyncEntities", JSON.stringify(Kinvey.DataStore.collection('customers').pendingSyncEntities()))
         console.log("pendingSyncCount", JSON.stringify(Kinvey.DataStore.collection('customers').pendingSyncCount()))
-        Kinvey.DataStore.collection('customers').clearSync();   
+        // Kinvey.DataStore.collection('customers').clearSync();
     }
-    
+
     public onItemTap(item) {
     	this.navigationService.navigate(
     		["../cmd-details", item._id],
     		{ relativeTo: this.activatedRoute }
     	);
     }
-    
+
     public createTapped() {
         this.navigationService.navigateTo(`../../fq`);
     }
